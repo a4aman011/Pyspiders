b01b3d53d1ba1438ae2498193bff952ee448f8df
Aman Ranjan <a4aman011@outlook.com>
2024-10-24 22:56:00 +0530

    added new patterns in pattern matching

--- Basics/patternMatching.py
+++ Basics/patternMatching.py
@@ -815,13 +815,220 @@
 #                 print("*",end=" ")
 #         print()
 
+# n: 3
+#     * 
+#   * * 
+# *   * 
+#   * * 
+#     * 
 
+# n=int(input("n: "))
+# for i in range(2*n-1):
+#         for j in range(n):
+#                 if i-j==n-1 or i+j==n-1 or j==n-1:
+#                         print("*",end=" ")
+#                 else:
+#                         print(" ",end=" ")
+#         print()
+
+# n: 3
+#  *      
+#  *   *    
+#  *     *  
+#  *   *    
+#  * 
+
+# n=int(input("n: "))
+# for i in range(2*n-1):
+#         for j in range(n):
+#                 if i==j or i+j==2*n-2 or j==0:
+#                         print("*",end=" ")
+#                 else:
+#                         print(" ",end=" ")
+#         print()
+
+# n: 3
+#     A 
+#   B B B 
+# C C C C C 
+
+# n=int(input("n: "))
+# val=0
+# for i in range(n):
+#         for j in range(n-i-1):
+#                 print(" ",end=" ")
+#         for k in range(2*i+1):
+#                 print(chr(65+val),end=" ")
+#         print()
+#         val+=1
+#         if val>25:
+#                 val=0
+
+# n: 3
+#     3 
+#   2 2 2 
+# 1 1 1 1 1 
+
+# n=int(input("n: "))
+# val=n
+# if val>9:
+#         val=9
+# for i in range(n):
+#         for j in range(n-i-1):
+#                 print(" ",end=" ")
+#         for k in range(2*i+1):
+#                 print(val,end=" ")
+#         print()
+#         val-=1
+#         if val<1:
+#                 val=9
+
+# n: 3
+#     1 
+#   2 3 4 
+# 5 6 7 8 9 
+
+# n=int(input("n: "))
+# val=1
+# if val>9:
+#         val=9
+# for i in range(n):
+#         for j in range(n-i-1):
+#                 print(" ",end=" ")
+#         for k in range(2*i+1):
+#                 print(val,end=" ")
+#                 val+=1
+#                 if val>9:
+#                         val=1
+#         print()
+
+# n: 4
+#       D 
+#     C C C 
+#   B B B B B 
+# A A A A A A A 
+
+# n=int(input("n: "))
+# val=n-1
+# if val>25:
+#         val=25
+# for i in range(n):
+#         for j in range(n-i-1):
+#                 print(" ",end=" ")
+#         for k in range(2*i+1):
+#                 print(chr(65+val),end=" ")
+#         print()
+#         val-=1
+#         if val<0:
+#                 val=25
+
+# n: 4
+# 1 2 3 4 5 6 7 
+#   8 9 1 2 3 
+#     4 5 6 
+#       7 
+
+# n=int(input("n: "))
+# val=1
+# if val<1:
+#         val=9
+# for i in range(n):
+#         for j in range(i):
+#                 print(" ",end=" ")
+#         for k in range(2*n-2*i-1):
+#                 print(val,end=" ")
+#                 val+=1
+#                 if val>9:
+#                         val=1
+#         print()
+
+# n: 4
+# * * * * * * * 
+#   * * * * * 
+#     * * * 
+#       * 
+
+# n=int(input("n: "))
+# for i in range(n):
+#         for j in range(i):
+#                 print(" ",end=" ")
+#         for k in range(2*n-2*i-1):
+#                 print("*",end=" ")
+#         print()
+
+# n=int(input("n: "))
+# val=1
+# for i in range(n):
+#         for j in range(n):
+#                 if i==j or i+j==n-1:
+#                         print(val,end=" ")
+#                 else:
+#                         print(" ",end=" ")
+#         print()
+#         val+=1
+#         if val>9:
+#                 val=1
+
+# n=int(input("n: "))
+# val=1
+# # p=True
+# for i in range(n):
+#         for j in range(n):
+#                 if i==j or i+j==n-1:
+#                         print("*",end=" ")
+#                 else:
+#                         print(" ",end=" ")
+#         print()
+
+
+# n=int(input("n: "))
+# val=1
+# for i in range(n):
+#         for j in range(2*n-1):
+#                 if i==j or i+j==2*n-2 or i==0:
+#                         print(val,end=" ")
+#                         val+=1
+#                 else:
+#                         print(" ",end=" ")
+#         print()
 
 n=int(input("n: "))
-for i in range(2*n-1):
+val=0
+if val<0:
+        val=25
+for i in range(n):
+        
         for j in range(n):
-                if i-j==n-1 or i+j==n-1 or j==n-1:
-                        print("*",end=" ")
+                if i+j>=n-1:
+                        if i%2==0:
+                                print(chr(65+val),end=" ")
+                                
+                        else:
+                                print("$",end=" ")
+                
+                                
                 else:
                         print(" ",end=" ")
+        val+=1        
         print()
+        
+        if val>25:
+                val=0
+
+# n=int(input("n: "))
+# val=0
+# if val<0:
+#         val=25
+# for i in range(n):
+#         for j in range(n):
+#                 if i+j>=n-1:
+#                         print(chr(65+val),end=" ")
+#                         print("$",end=" ")
+#                 else:
+#                         print(" ",end=" ")
+                
+#         print()
+#         val+=1
+#         if val>25:
+#                 val=0
+
